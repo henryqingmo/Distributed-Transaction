2fc50170b9b9498283b6286edc9b84b4a69f0db4
henryqingmo <henryqingmo@gmail.com>
2026-04-24 17:22:50 -0500

    impl balance, deposit and withdraw on participant

--- internal/server/participant.go
+++ internal/server/participant.go
@@ -16,3 +16,35 @@ const (
 	VoteYes Vote = iota
 	VoteNo
 )
+
+func (s *Server) getBalance(txnID, account string) (int, bool) {
+	if tentative, exists := s.TentativeWrites[txnID][account]; exists {
+		return tentative, true
+	}
+	acct, found := s.Accounts[account]
+	if !found {
+		return 0, false
+	}
+	return acct.CommittedBalance, true
+}
+
+func (s *Server) execDeposit(txnID, account string, amount int) {
+	if s.TentativeWrites[txnID] == nil {
+		s.TentativeWrites[txnID] = make(map[string]int)
+	}
+	curr, _ := s.getBalance(txnID, account)
+	s.TentativeWrites[txnID][account] = curr + amount
+}
+
+// Returns false if account not found (triggers abort)
+func (s *Server) execWithdraw(txnID, account string, amount int) bool {
+	curr, found := s.getBalance(txnID, account)
+	if !found {
+		return false
+	}
+	if s.TentativeWrites[txnID] == nil {
+		s.TentativeWrites[txnID] = make(map[string]int)
+	}
+	s.TentativeWrites[txnID][account] = curr - amount
+	return true
+}
